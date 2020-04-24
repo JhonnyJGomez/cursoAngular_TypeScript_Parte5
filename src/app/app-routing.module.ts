0230fa32b3181d0537ce1cae0809f8c24cc9cdf4
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-24 18:33:15 -0500

    Se agrega componentes encabezado, pedidos, clientes, productos. + Routerlink, + bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { ProductosComponent } from './productos/productos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ClientesComponent },
+  { path: 'productos', component: ProductosComponent  },
+  { path: 'pedidos', component: PedidosComponent  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ProductosComponent } from './productos/productos.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    ProductosComponent,
+    PedidosComponent,
+    EncabezadoComponent
   ],
   imports: [
     BrowserModule,
