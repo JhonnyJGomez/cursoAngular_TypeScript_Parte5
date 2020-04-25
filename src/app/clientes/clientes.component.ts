7076c6f50edf2f29e2b90c0a5a8e2cca0167ba82
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-24 20:19:12 -0500

    LocalStorage: se gurgar y lee usando setItem, getItem

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Clientes{
+  nombre:string,
+  apellido:string,
+  edad:number  
+}
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
+  clientes:Array<Clientes> = new Array<Clientes>()
+
   constructor() { }
 
   ngOnInit(): void {
+   this.clientes.push(
+       {
+        nombre:"Maia",
+        apellido:"Gómez Olaguibel",
+        edad:2
+      },
+      {
+        nombre:"Karen",
+        apellido:"Olaguibel Rossi",
+        edad:29
+      },
+      {
+        nombre:"Jhonny",
+        apellido:"Gómez Morales",
+        edad:30
+      })
   }
 
+  guardar(){
+    localStorage.setItem("cliente", JSON.stringify(this.clientes))
+  }
+
+  leer(){
+    this.clientes = JSON.parse(localStorage.getItem("cliente"))
+    console.log(JSON.parse(localStorage.getItem("cliente")))
+  }
 }
