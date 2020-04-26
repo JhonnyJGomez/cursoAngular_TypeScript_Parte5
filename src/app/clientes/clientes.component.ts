899bba50090c080691584aeb12745b426760981a
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-25 19:22:09 -0500

    LocalStorage: se eliminan registros usando Delete Item

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -4,6 +4,10 @@ interface Clientes{
   apellido:string,
   edad:number  
 }
+interface Productos{
+  nombre:string,
+  precio:number
+}
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
@@ -12,11 +16,11 @@ interface Clientes{
 export class ClientesComponent implements OnInit {
 
   clientes:Array<Clientes> = new Array<Clientes>()
-
+  productos:Array<Productos> = new Array<Productos>()
   constructor() { }
 
   ngOnInit(): void {
-   this.clientes.push(
+  /*  this.clientes.push(
        {
         nombre:"Maia",
         apellido:"Gómez Olaguibel",
@@ -32,14 +36,45 @@ export class ClientesComponent implements OnInit {
         apellido:"Gómez Morales",
         edad:30
       })
+       
+    this.productos.push(
+      {
+        nombre:"Arroz",
+        precio:2000
+      },
+      {
+        nombre:"Maiz",
+        precio:2100
+      },
+      {
+        nombre:"pan",
+        precio:300
+      }) */
   }
 
-  guardar(){
-    localStorage.setItem("cliente", JSON.stringify(this.clientes))
+  guardarClientes(){
+    localStorage.setItem("clientes", JSON.stringify(this.clientes))
+  }
+
+  guardarProductos(){
+    localStorage.setItem("productos", JSON.stringify(this.productos))
   }
 
   leer(){
-    this.clientes = JSON.parse(localStorage.getItem("cliente"))
-    console.log(JSON.parse(localStorage.getItem("cliente")))
+    this.clientes = JSON.parse(localStorage.getItem("clientes"))
+    this.productos = JSON.parse(localStorage.getItem("productos"))
   }
+
+  eliminarClientes(){
+    localStorage.removeItem("clientes")
+  }
+
+  eliminarProductos(){
+    localStorage.removeItem("productos")
+  }
+
+  eliminarTodo(){
+    localStorage.clear()
+  }
+
 }
