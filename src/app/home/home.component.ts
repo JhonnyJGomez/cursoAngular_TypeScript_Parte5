38da52db635710ff1f91944461dc08781a2ce7b9
Jhonny Gomez <jg.jhonny90@gmail.com>
2020-04-30 23:04:23 -0500

    LocalStorage: se agrega buscador de clientes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from '../services/clientes.service';
+import { Clientes } from '../models/clientes';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  clienteArray:Array<Clientes> = new Array<Clientes>()
+  constructor(public clienteInyectado:ClientesService) { }
 
   ngOnInit(): void {
+    this.clienteArray = this.clienteInyectado.clientesLocalStorage;
+  }
+
+  buscarCliente(nombre:string){
+    this.clienteArray = this.clienteInyectado.clientesLocalStorage.filter(cliente=>{
+      return cliente.nombre.toLocaleLowerCase().includes(nombre.toLocaleLowerCase())
+    })
+
   }
 
 }
